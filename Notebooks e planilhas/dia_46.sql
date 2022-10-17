10b2b68232f7e1f16544238fd6251db94b67acbb
Rafaela R <99053447+ribeirorafaela@users.noreply.github.com>
2022-10-17 20:35:05 -0300

    Create dia_46.sql

---
+++ Notebooks e planilhas/dia_46.sql
@@ -0,0 +1,65 @@
+/* Faturamento Anual */
+select year(data_venda), sum(quantidade * preco) as faturamento
+from notas_fiscais NF inner join itens_notas_fiscais inf
+on nf.numero = inf.NUMERO
+group by year(DATA_VENDA)
+
+/* Quantos clientes cadastrados */
+SELECT COUNT(*) FROM TABELA_DE_CLIENTES;
+
+/* Quantos clientes compraram */
+SELECT CPF, COUNT(*) FROM notas_fiscais GROUP BY CPF;
+
+SELECT a.cpf, a.nome, b.cpf, b.nome from TABELA_DE_CLIENTES a 
+inner join notas_fiscais b 
+on a.cpf = b.cpf;
+
+
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A
+INNER JOIN notas_fiscais B ON A.CPF = B.CPF;
+
+
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A
+LEFT JOIN notas_fiscais B ON A.CPF = B.CPF
+WHERE B.CPF IS NULL AND YEAR(B.DATA_VENDA) = 2015;
+
+
+
+SELECT * FROM tabela_de_vendedores;
+
+SELECT count(*) FROM tabela_de_clientes;
+
+SELECT * FROM tabela_de_vendedores INNER JOIN tabela_de_clientes
+ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+
+SELECT tabela_de_vendedores.BAIRRO,
+tabela_de_vendedores.NOME, DE_FERIAS,
+tabela_de_clientes.BAIRRO,
+tabela_de_clientes.NOME  FROM tabela_de_vendedores INNER JOIN tabela_de_clientes
+ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+
+SELECT tabela_de_vendedores.BAIRRO,
+tabela_de_vendedores.NOME, DE_FERIAS,
+tabela_de_clientes.BAIRRO,
+tabela_de_clientes.NOME  FROM tabela_de_vendedores LEFT JOIN tabela_de_clientes
+ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+
+SELECT tabela_de_vendedores.BAIRRO,
+tabela_de_vendedores.NOME, DE_FERIAS,
+tabela_de_clientes.BAIRRO,
+tabela_de_clientes.NOME  FROM tabela_de_vendedores RIGHT JOIN tabela_de_clientes
+ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+
+SELECT tabela_de_vendedores.BAIRRO,
+tabela_de_vendedores.NOME, DE_FERIAS,
+tabela_de_clientes.BAIRRO,
+tabela_de_clientes.NOME  FROM tabela_de_vendedores FULL JOIN tabela_de_clientes
+ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+
+SELECT tabela_de_vendedores.BAIRRO,
+tabela_de_vendedores.NOME, DE_FERIAS,
+tabela_de_clientes.BAIRRO,
+tabela_de_clientes.NOME  FROM tabela_de_vendedores , tabela_de_clientes;
+
+
+
